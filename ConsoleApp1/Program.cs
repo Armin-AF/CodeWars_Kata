6047f6b82ca36115cbea4edbfd2d2c20cd2a9d24
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-10-13 21:59:44 +0200

    More Kata

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using LeetCode;
+
+class Program
+{
+    static void Main()
+    {
+        var myHashTable = new Hashtable{
+            {"bob", 27},
+            {33, "fred"}
+        };
+
+        int theAge = (int)(myHashTable["bob"] ?? throw new InvalidOperationException());
+        
+        Console.WriteLine(theAge);
+        
+        IDictionaryEnumerator myEnumerator = myHashTable.GetEnumerator();
+
+        while (myEnumerator.MoveNext())
+        {
+            Console.WriteLine("{0}: {1}", myEnumerator.Key, myEnumerator.Value);
+        }
+
+        StringBuilder myStringBuilder = new StringBuilder();
+
+        myStringBuilder.Append("bob");
+        myStringBuilder.Append(" smith");
+        myStringBuilder.Append(" jr");
+        myStringBuilder.Insert(2, " ");
+        var hashCode = myStringBuilder.GetHashCode();
+        Console.WriteLine(hashCode);
+
+        string myString = myStringBuilder.ToString();
+        
+        Console.WriteLine(myString);
+
+        
+        var idk = Class1.PigIt("Pig latin is cool");
+        Console.WriteLine(idk);
+
+    }
+}

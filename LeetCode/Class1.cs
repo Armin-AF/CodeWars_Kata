22ee440b8c595d5d8be5a9fe9abad513de0be7e6
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-03-22 11:15:24 +0100

    ClosestPair Kata
    ClosestPair Kata

--- LeetCode/Class1.cs
+++ LeetCode/Class1.cs
@@ -1977,6 +1977,26 @@ public static class Class1
 
         return result;
     }
+    
+    public static float[] ClosestPair(float[][] array){
+        // takes an array of arrays of floats and returns an array of floats that represents the closest pair of the array.
+        var result = new float[4];
+        var min = float.MaxValue;
+        for (var i = 0; i < array.Length; i++){
+            for (var j = i + 1; j < array.Length; j++){
+                var distance = (float)Math.Sqrt(Math.Pow(array[i][0] - array[j][0], 2) + Math.Pow(array[i][1] - array[j][1], 2));
+                if (distance < min){
+                    min = distance;
+                    result[0] = array[i][0];
+                    result[1] = array[i][1];
+                    result[2] = array[j][0];
+                    result[3] = array[j][1];
+                }
+            }
+        }
+
+        return result;
+    }
 
 }
 
